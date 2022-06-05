23e00a34b49f02e4f3bfd8a8978ffb34f732815d
eugenesvk <eugenesvk@users.noreply.github.com>
2022-06-05 16:52:28 +0700

    _BUG_ tempfix: disable kolorist until a ESM PR is merged
    https://github.com/marvinhagemeister/kolorist/pull/14

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path'
-import { bgCyan, black } from 'kolorist'
+// import { bgCyan, black } from 'kolorist' // breaks with "moduleResolution": "nodenext", PR https://github.com/marvinhagemeister/kolorist/pull/14
 
 export const port = parseInt(process.env.PORT || '') || 3303
 export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
@@ -8,5 +8,6 @@ export const isWin = process.platform === "win32";
 
 export function log(name: string, message: string) {
   // eslint-disable-next-line no-console
-  console.log(black(bgCyan(` ${name} `)), message)
+  // console.log(black(bgCyan(` ${name} `)), message)
+  console.log(` ${name} `, message)
 }
