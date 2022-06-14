19907e5d5750f59442fc352ab27e45f962a192d4
eugenesvk <eugenesvk@users.noreply.github.com>
2022-06-15 00:11:43 +0700

    vite: fix a typo

--- vite.config.ts
+++ vite.config.ts
@@ -51,7 +51,7 @@ export const sharedConfig	: UserConfig = {
 
 export default defineConfig(({ command }) => ({
   ...sharedConfig,
-  base    	: command === 'serve' ? `http	://localhost	:${port}/`	: '/dist/',
+  base    	: command === 'serve' ? `http://localhost:${port}/` : '/dist/',
   server  	: {
     port  	,
     hmr   	: {
