97e3f450facfd41d3d8e7858f96d486af9d0e1e1
eugenesvk <eugenesvk@users.noreply.github.com>
2022-06-15 18:43:04 +0700

    remove existing element text
    previously vue's app.mount did this automatically

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -2,4 +2,5 @@ import { render }	from 'solid-js/web'
 import App       	from './Popup'
 import           	     '../styles'
 
+document.getElementById('app').textContent = '';
 render(App, document.getElementById('app'));
