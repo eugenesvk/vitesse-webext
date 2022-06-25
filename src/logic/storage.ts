37cfc046166a297fb6da36b774abf45788e57295
eugenesvk <eugenesvk@users.noreply.github.com>
2022-06-25 17:31:19 +0700

    fix warning about async storage computations being created outside a reactive context
    Add a rootless primitives dep and use it to create a Shared Root (it gets created when the returned function gets first called, and disposed when last reactive context listening to it gets disposed)

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,11 +1,15 @@
-import { useStorageLocal, storageLocalAPI } from '~/composables/useStorageLocal'
+import { useStorageLocal, storageLocalAPI }	from '~/composables/useStorageLocal'
+import {createSharedRoot}                  	from '@solid-primitives/rootless'
 
 const prefix 	= 'vitesse';
 const key    	= 'webext_demo';
 const prefkey	= [prefix,key].join('.');
 
-const [storageDemo, setStorageDemo, storageDemoAct
-  ] = useStorageLocal({api:storageLocalAPI, prefix:prefix, sync:true});
+const useState = createSharedRoot(() => {
+  return useStorageLocal({api:storageLocalAPI, prefix:prefix, sync:true});
+});
+
+const [storageDemo, setStorageDemo, storageDemoAct] = useState()
 
 if (typeof await storageDemo[key] === 'undefined') { await setStorageDemo(key, 'Storage Demo'); }
 export { storageDemo, setStorageDemo, storageDemoAct, prefix, key, prefkey}
